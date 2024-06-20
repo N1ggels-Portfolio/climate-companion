3dd539ce875a12bab57dfc6f945e95466c6c4529
Niclas Niegsch <niegsch@louis.info>
2024-06-20 09:31:19 +0200

    Added cards background depending on weather condition

---
+++ assets/ts/api/meteomatics.ts
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { getToken } from "../auth/auth";
+import { getLocation } from "./opencagedata";
+import { ref } from "vue";
+
+export async function getData(inputValue: Ref<string>) {
+  try {
+    const token = await getToken(); // Token for ajax call
+    const locationInfo = await getLocation(inputValue) as any; // Get location information
+    const location = ref('');
+    const info = ref([]);
+    
+    if (locationInfo.components.city) {
+      location.value = locationInfo.components.city;
+    } else if (locationInfo.components.village) {
+      location.value = locationInfo.components.village;
+    } else if (locationInfo.formatted) {
+      location.value = locationInfo.formatted;
+    }
+    
+    const lat = `${locationInfo.geometry.lat}`;
+    const lng = `${locationInfo.geometry.lng}`;
+    const timeSpan = createTimeSpan();
+    const interval = 1; // Output time interval in hours
+
+    const url = `https://api.meteomatics.com/${timeSpan}:PT${interval}H/t_2m:C,precip_1h:mm,wind_speed_10m:ms,weather_symbol_${interval}h:idx/${lat},${lng}/json?model=mix&access_token=${token}`;
+
+    const { data } = await axios.get(url);
+    const weatherCast = data.data;
+    const temp = weatherCast[0].coordinates[0].dates;
+    const precip = weatherCast[1].coordinates[0].dates;
+    const wind = weatherCast[2].coordinates[0].dates;
+    const condition = weatherCast[3].coordinates[0].dates;
+
+
+    // Create a new array to save all corresponding values together
+    info.value = temp.map((data: any, index: any) => ({
+      temp: data,
+      precip: precip[index],
+      wind: wind[index],
+      condition: condition[index]
+    }));
+
+    return { location, info };
+  } catch (error) {
+    console.error('There has beem a problem with your request: ', error)
+  }
+
+  // Create the timespan for the api call
+  function createTimeSpan(): string {
+    // Get the current date and time
+    const now = new Date();
+
+    // Calculate the start time (last 3 hours)
+    const startTime = new Date(now.getTime() - 3 * 60 * 60 * 1000);
+    // Format the start time
+    const formattedStartTime = startTime.toISOString().slice(0, 19).replace('T', 'T') + 'Z';
+
+    // Calculate the end time
+    const endTime = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+    // Format the end time
+    const formattedEndTime = endTime.toISOString().slice(0, 19).replace('T', 'T') + 'Z';
+
+    // Create the URL string
+    return `${formattedStartTime}--${formattedEndTime}`;
+  }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <main class="main">  
     <section class="section section__cards">
       <div class="card card__location" v-if="locations.length" v-for="(item, index) in locations" :key="index">
+        <div class="background" v-html="renderWeatherContainer(getWeatherConditionDescription(item.info[3].condition.value))"></div>
         <div class="card__image"></div>
         <div class="card__location-name" v-if="item.location">{{ item.location }}</div>
         <div class="card__content">
@@ -98,19 +99,19 @@ async function saveLocation(name: string) {
 async function getSavedLocations() {
   try {
     const response = await fetch ('http://127.0.0.1:5000/get_locations');
-
+    
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    
+      
     const data = await response.json();
-
-    if (!data.fetched) return;
-
+    
+    if (data.fetched == false) return;
+    
     const fetchDataPromises = data.map(async (item: any) => {
       return await fetchData(item.name);
     })
-
+      
     locations.value = await Promise.all(fetchDataPromises);
   } catch (error) {
     console.error('Error:', error);
@@ -118,6 +119,217 @@ async function getSavedLocations() {
 }
 
 onMounted(getSavedLocations);
+
+// Define the mapping for daytime and nighttime condition
+const weatherConditions = {
+  '1': { 'daytime': 'clear-sky', 'nighttime': 'clear-sky' },
+  '2': { 'daytime': 'light-clouds', 'nighttime': 'light-clouds' },
+  '3': { 'daytime': 'partly-cloudy', 'nighttime': 'partly-cloudy' },
+  '4': { 'daytime': 'cloudy', 'nighttime': 'cloudy' },
+  '5': { 'daytime': 'rain', 'nighttime': 'rain' },
+  '6': { 'daytime': 'sleet', 'nighttime': 'sleet' },
+  '7': { 'daytime': 'snow', 'nighttime': 'snow' },
+  '8': { 'daytime': 'rain-shower', 'nighttime': 'rain-shower' },
+  '9': { 'daytime': 'snow-shower', 'nighttime': 'snow-shower' },
+  '10': { 'daytime': 'sleet-shower', 'nighttime': 'sleet-shower' },
+  '11': { 'daytime': 'light-fog', 'nighttime': 'light-fog' },
+  '12': { 'daytime': 'dense-fog', 'nighttime': 'dense-fog' },
+  '13': { 'daytime': 'freezing-rain', 'nighttime': 'freezing-rain' },
+  '14': { 'daytime': 'thunderstorms', 'nighttime': 'thunderstorms' },
+  '15': { 'daytime': 'drizzle', 'nighttime': 'drizzle' },
+  '16': { 'daytime': 'sandstorm', 'nighttime': 'sandstorm' }
+};
+
+// Function to get the weather condition description based on the code
+function getWeatherConditionDescription(code: number): string {
+  // Determine if it's daytime or nighttime based on the code
+  const isDaytime = parseInt(code) >= 1 && parseInt(code) <= 16;
+
+  if (code > 17) {
+    code = code - 100;
+  }
+
+  const condition = weatherConditions[code];
+
+  if (!condition) {
+    return 'Unknown condition';
+  }
+  return isDaytime? condition['daytime'] : condition['nighttime'];
+}
+
+function renderWeatherContainer(conditionDescription) {
+  switch (conditionDescription) {
+    case "clear-sky":
+      return `
+      <div class="weather-container clear-day">
+        <div class="sun"></div>
+        <div class="sun-ray r-one"></div>
+        <div class="sun-ray r-two"></div>
+        <div class="sun-ray r-three"></div>
+        <div class="sun-ray r-four"></div>
+        <div class="cloud">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+      </div>
+      `;
+    case "light-clouds":
+      return `
+        <div class="weather-container partly-cloudy">
+          <div class="cloudleft">
+            <div class="circle c-one"></div>
+            <div class="circle c-two"></div>
+          </div>
+          <div class="cloudbottom">
+            <div class="circle c-one"></div>
+            <div class="circle c-two"></div>
+            <div class="circle c-three"></div>
+            <div class="circle c-four"></div>
+          </div>
+          <div class="cloudright">
+            <div class="circle c-one"></div>
+          </div>
+        </div>
+        `;
+    case "partly-cloudy":
+      return `
+      <div class="weather-container partly-cloudy">
+        <div class="cloudleft">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+        </div>
+        <div class="cloudbottom">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+          <div class="circle c-four"></div>
+        </div>
+        <div class="cloudright">
+          <div class="circle c-one"></div>
+        </div>
+      </div>
+      `;
+    case "cloudy":
+      return `
+      <div class="weather-container cloudy">
+        <div class="cloud-one">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-two">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-three">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-four">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-five">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-six">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-seven">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-eight">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-nine">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-ten">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-eleven">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-twelve">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+      </div>`;
+    case "rain":
+      return `
+      <div class="weather-container rain">
+        <div class="cloud-one">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+          <div class="circle c-four"></div>
+        </div>
+        <div class="cloud-two">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="cloud-three">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+        </div>
+        <div class="droplet-container"></div>
+      </div>
+      `;
+    case "sleet":
+      return '<div class="weather-container sleet">...</div>';
+    case "snow":
+      return `
+      <div class="weather-container snow">
+        <div class="cloud-one">
+          <div class="circle c-one"></div>
+          <div class="circle c-two"></div>
+          <div class="circle c-three"></div>
+        </div>
+        <div class="snowflake-container"></div>
+      </div>
+      `;
+    case "rain-shower":
+      return '<div class="weather-container rain-shower">...</div>';
+    case "snow-shower":
+      return '<div class="weather-container snow-shower">...</div>';
+    case "sleet-shower":
+      return '<div class="weather-container sleet-shower">...</div>';
+    case "light-fog":
+      return '<div class="weather-container light-fog">...</div>';
+    case "dense-fog":
+      return '<div class="weather-container dense-fog">...</div>';
+    case "freezing-rain":
+      return '<div class="weather-container freezing-rain">...</div>';
+    case "thunderstorms":
+      return '<div class="weather-container thunderstorms">...</div>';
+    case "drizzle":
+      return '<div class="weather-container drizzle">...</div>';
+    case "sandstorm":
+      return '<div class="weather-container sandstorm">...</div>';
+    default:
+      return '';
+  }
+}
 
 
 // Modal logic
@@ -154,6 +366,7 @@ const openModal = () => {
 }
 
 .card {
+  position: relative;
 
   padding: .5rem 1rem;
   width: 17rem;
@@ -218,6 +431,11 @@ const openModal = () => {
     align-items: center;
 
     cursor: pointer;
+  }
+
+  .background {
+    position: absolute;
+    inset: -1rem;
   }
 }
 </style>
